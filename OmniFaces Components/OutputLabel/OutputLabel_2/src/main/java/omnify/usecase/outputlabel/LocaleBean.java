2d76a84e0a9d006b73bc760d2effeaa6e0e20e9c
Constantin Alin <constantin_alin_marian@yahoo.com>
2015-07-06 11:18:54 +0300

    Omnify eliminating unnecessary label duplication ("dynamic" labels)

---
+++ OmniFaces Components/OutputLabel/OutputLabel_2/src/main/java/omnify/usecase/outputlabel/LocaleBean.java
@@ -0,0 +1,25 @@
+package omnify.usecase.outputlabel;
+
+import java.io.Serializable;
+import java.util.Locale;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Constantin Alin
+ */
+@Named
+@RequestScoped
+public class LocaleBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	public void setCurrentLocale(String locale) {
+		UIViewRoot viewRoot = FacesContext.getCurrentInstance().getViewRoot();
+		viewRoot.setLocale(new Locale(locale));
+	}
+
+}
