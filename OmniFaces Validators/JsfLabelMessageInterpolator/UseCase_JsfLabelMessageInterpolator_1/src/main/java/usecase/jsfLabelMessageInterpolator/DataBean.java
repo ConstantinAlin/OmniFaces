bbc65f3bdff04c9cd9182a5f63ac15c6a2a8d4ea
Constantin Alin <constantin_alin_marian@yahoo.com>
2015-07-08 08:36:01 +0300

    Placing the label anywhere in the message body

---
+++ OmniFaces Validators/JsfLabelMessageInterpolator/UseCase_JsfLabelMessageInterpolator_1/src/main/java/usecase/jsfLabelMessageInterpolator/DataBean.java
@@ -0,0 +1,38 @@
+package usecase.jsfLabelMessageInterpolator;
+
+import java.io.Serializable;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author Constantin Alin
+ */
+@Named
+@RequestScoped
+public class DataBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Size(min = 2, max = 25)
+	private String name;
+	@Size(min = 2, max = 25)
+	private String surname;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+}
