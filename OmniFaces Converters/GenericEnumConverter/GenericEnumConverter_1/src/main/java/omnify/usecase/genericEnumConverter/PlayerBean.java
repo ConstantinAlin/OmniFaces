2fb7025d0eda8ddfb7a1dc199267e64b916705d9
Constantin Alin <constantin_alin_marian@yahoo.com>
2015-07-08 08:23:45 +0300

    Omnify preserving items type over submit (avoiding them to default to String)

---
+++ OmniFaces Converters/GenericEnumConverter/GenericEnumConverter_1/src/main/java/omnify/usecase/genericEnumConverter/PlayerBean.java
@@ -0,0 +1,36 @@
+package omnify.usecase.genericEnumConverter;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Constantin Alin
+ */
+@Named
+@RequestScoped
+public class PlayerBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private List<PlayerEnum> players;
+	private PlayerEnum selectedPlayerEnum;
+
+	public List<PlayerEnum> getPlayers() {
+		return players;
+	}
+
+	public void setPlayers(List<PlayerEnum> players) {
+		this.players = players;
+	}
+
+	public PlayerEnum getSelectedPlayerEnum() {
+		return selectedPlayerEnum;
+	}
+
+	public void setSelectedPlayerEnum(PlayerEnum selectedPlayerEnum) {
+		this.selectedPlayerEnum = selectedPlayerEnum;
+	}
+}
