fe1b7b55b9fbc3497df6560911bfd2a11283b427
Constantin Alin <constantin_alin_marian@yahoo.com>
2015-07-26 10:53:55 +0300

    Suppressing the invocation of the setter/converter/validatator at each postback

---
+++ OmniFaces Components/ViewParam/UseCase_ViewParam_1/src/main/java/usecase/viewparam/PhoneNumberValidator.java
@@ -0,0 +1,33 @@
+package usecase.viewparam;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+/**
+ *
+ * @author Constantin Alin
+ */
+@FacesValidator
+public class PhoneNumberValidator implements Validator {
+
+	private static final Logger LOG = Logger.getLogger(PhoneNumberValidator.class.getName());
+
+	@Override
+	public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+
+		LOG.log(Level.INFO, "Validator called for: {0} with value: {1}", new Object[] { component.toString(), value });
+
+		if (value instanceof PhoneNumber == false) {
+			FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, null,
+					"The phone number is not correct!");
+			throw new ValidatorException(message);
+		}
+	}
+
+}
