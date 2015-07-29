0665c0b17b2b751020d3a6b018b768abc44b5e88
Constantin Alin <constantin_alin_marian@yahoo.com>
2015-07-29 07:37:08 +0300

    Omnify preserving view parameters over postbacks

---
+++ OmniFaces Components/Form (& IgnoreValidationFailed)/Form_2/src/main/java/omnify/usecase/form/DataBean.java
@@ -0,0 +1,80 @@
+package omnify.usecase.form;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Constantin Alin
+ */
+@Named
+@RequestScoped
+public class DataBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	public DataBean() {
+		// NOOP
+	}
+
+	private String name;
+	private String surname;
+	private String phone;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = 7;
+		hash = 79 * hash + Objects.hashCode(this.name);
+		hash = 79 * hash + Objects.hashCode(this.surname);
+		hash = 79 * hash + Objects.hashCode(this.phone);
+		return hash;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final DataBean other = (DataBean) obj;
+		if (!Objects.equals(this.name, other.name)) {
+			return false;
+		}
+		if (!Objects.equals(this.surname, other.surname)) {
+			return false;
+		}
+		if (!Objects.equals(this.phone, other.phone)) {
+			return false;
+		}
+		return true;
+	}
+
+}
