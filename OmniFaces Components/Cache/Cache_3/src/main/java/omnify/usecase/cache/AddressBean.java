20d6d633c7bddc09e18a3765a062d674f4f11bd1
Constantin Alin <constantin_alin_marian@yahoo.com>
2015-07-29 07:31:29 +0300

    Omnify caching a fragment of render markup 3

---
+++ OmniFaces Components/Cache/Cache_3/src/main/java/omnify/usecase/cache/AddressBean.java
@@ -0,0 +1,47 @@
+package omnify.usecase.cache;
+
+import java.io.Serializable;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Constantin Alin
+ */
+@Named
+@ViewScoped
+public class AddressBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private List<String> addresses = new ArrayList<>();
+	private static final String[] addressesArray = new String[] { "Monte-Carlo, Monaco", "Bottmingen, Switzerland",
+			"Manacor, Mallorca, Spain", "St. Barthelemy, Switzerland", "Bradenton, Florida, USA", "London, England",
+			"Jurmala, Latvia", "Delray Beach, FL, USA / Johann", "Neuchatel, Switzerland", "La Roda (Albacete), Spain",
+			"Salto, R. Oriental del Uruguay", "Madrid, Spain", "Liege, Belgium", "Canberra and Melbourne, Austra",
+			"Astana, Kazakhstan" };
+
+	public AddressBean() {
+		add();
+	}
+
+	public List<String> getAddresses() {
+		return addresses;
+	}
+
+	public void setAddresses(List<String> addresses) {
+		this.addresses = addresses;
+	}
+
+	public void generateAddress() throws ParseException {
+		add();
+	}
+
+	private void add() {
+		addresses.add(addressesArray[new Random().nextInt(addressesArray.length)]);
+	}
+}
